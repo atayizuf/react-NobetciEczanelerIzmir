bad6b7d2ee85d7cf1a9f7673f4538f7b0e2bd62d
Ufuk ACIKGOZ <ufuk.acikgoz@hotmail.com>
2023-02-18 15:33:57 +0300

    kartduzenleme

--- src/components/Kart.js
+++ src/components/Kart.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./kart.css";
 
 function Kart(props) {
   return (
     <div>
       {props.veriler.map((x) => (
-        <div>{x.Bolge}</div>
+        <>
+          <div className="kart">
+            <div className="bolge">{x.Bolge}</div>
+            <div className="ad">{x.Adi}</div>
+            <div className="tel">{x.Telefon}</div>
+            <div className="adres">{x.Adres}</div>
+            {x.BolgeAciklama && (
+              <div className="aciklama">{x.BolgeAciklama}</div>
+            )}
+          </div>
+        </>
       ))}
     </div>
   );
