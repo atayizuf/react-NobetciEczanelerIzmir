44d464a47f8975219d3a6a0165004844af411e07
Ufuk ACIKGOZ <ufuk.acikgoz@hotmail.com>
2023-04-03 23:05:12 +0300

    listKeyEkleme

--- src/components/Kart.js
+++ src/components/Kart.js
@@ -6,31 +6,29 @@ function Kart(props) {
     <>
       <div className="kapskart">
         {props.veri2.map((x) => (
-          <>
-            <div className="kart">
-              <div className="kartyazi">
-                <div className="bolge">{x.Bolge}</div>
-                <div className="ad">{x.Adi}</div>
-                <div className="tel">{x.Telefon}</div>
-                <div className="adres">{x.Adres}</div>
-                {x.BolgeAciklama && (
-                  <div className="aciklama">*** {x.BolgeAciklama} ***</div>
-                )}
-              </div>
-              <div
-                className="kartbutton"
-                onClick={() => {
-                  window.open(
-                    `https://www.google.com/maps/place/${x.LokasyonX},${x.LokasyonY}/@${x.LokasyonX},${x.LokasyonY},17.25z`,
-                    // `https://www.google.com/maps/place/1566+sokak+doğanlar+İzmir+/@${x.LokasyonX},${x.LokasyonY},17.25z`,
-                    "_blank"
-                  );
-                }}
-              >
-                Bas git!
-              </div>
+          <div className="kart" key={x.LokasyonX}>
+            <div className="kartyazi">
+              <div className="bolge">{x.Bolge}</div>
+              <div className="ad">{x.Adi}</div>
+              <div className="tel">{x.Telefon}</div>
+              <div className="adres">{x.Adres}</div>
+              {x.BolgeAciklama && (
+                <div className="aciklama">*** {x.BolgeAciklama} ***</div>
+              )}
             </div>
-          </>
+            <div
+              className="kartbutton"
+              onClick={() => {
+                window.open(
+                  `https://www.google.com/maps/place/${x.LokasyonX},${x.LokasyonY}/@${x.LokasyonX},${x.LokasyonY},17.25z`,
+                  // `https://www.google.com/maps/place/1566+sokak+doğanlar+İzmir+/@${x.LokasyonX},${x.LokasyonY},17.25z`,
+                  "_blank"
+                );
+              }}
+            >
+              Bas git!
+            </div>
+          </div>
         ))}
       </div>
     </>
